ddd39b03c2411875ee878c635e25f15595c89b59
Sashank Solasa <sashanksolasa@gmail.com>
2026-05-20 09:54:28 +0530

    Time: 3 ms (59.15%) | Memory: 43.8 MB (38.06%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -10,10 +10,10 @@ class Solution {
             }
         }
         StringBuilder sb=new StringBuilder();
-        for(char c : st){
-            sb.append(c);
+        while(!st.isEmpty()){
+            sb.append(st.pop());
         }
 
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
