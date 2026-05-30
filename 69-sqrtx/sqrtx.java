f1e6ef1337a35cb8aeb4e5a8bc6558e4d882ec5c
Sashank Solasa <sashanksolasa@gmail.com>
2026-05-31 00:02:15 +0530

    Time: 1 ms (98.05%) | Memory: 42.6 MB (41.05%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,5 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)(Math.sqrt(x));
+        int left=1;
+        int right=x;
+       while(left<=right){
+        int mid=left+(right-left)/2;
+        long square=(long)mid*mid;
+        if(square<=x){
+            left=mid+1;
+        }
+        else{
+            right=mid-1;
+        }
+
+       }
+        return right;
     }
 }
