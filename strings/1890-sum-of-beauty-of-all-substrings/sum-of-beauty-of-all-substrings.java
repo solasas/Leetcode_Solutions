3f98565440f80d4ac06e500514c20e7c26df2a31
Sashank Solasa <sashanksolasa@gmail.com>
2026-06-01 00:51:52 +0530

    Time: 59 ms (91.61%) | Memory: 44 MB (70.14%) - LeetSync

---
+++ strings/1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,56 @@
+class Solution {
+    // public int beautySum(String s) {
+    //     int n = s.length();
+    //     int sum = 0;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=i;j<n;j++){
+    //             String sub=s.substring(i,j+1);
+    //             int[] freq=new int[26];
+    //             for(int k=0;k<sub.length();k++){
+    //                 freq[sub.charAt(k)-'a']++;
+    //             }
+    //             int min_freq=Integer.MAX_VALUE;
+    //             int max_freq=Integer.MIN_VALUE;
+    //             for(int k=0;k<26;k++){
+    //                 if(freq[k]>0){
+    //                 min_freq=Math.min(min_freq,freq[k]);
+    //                 max_freq=Math.max(max_freq,freq[k]);
+    //                 }
+    //             }
+    //             sum+=(max_freq-min_freq);
+    //         }
+    //     }
+    //     return sum;
+    // }
+   
+    public int beautySum(String s) {
+        int n = s.length();
+        int sum = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            int[] freq = new int[26];
+
+            for(int j = i; j < n; j++) {
+
+                freq[s.charAt(j) - 'a']++;
+
+                int maxFreq = Integer.MIN_VALUE;
+                int minFreq = Integer.MAX_VALUE;
+
+                for(int k = 0; k < 26; k++) {
+
+                    if(freq[k] > 0){
+                        maxFreq = Math.max(maxFreq, freq[k]);
+                        minFreq = Math.min(minFreq, freq[k]);
+                    }
+                }
+
+                sum += (maxFreq - minFreq);
+            }
+        }
+
+        return sum;
+    }
+
+}
